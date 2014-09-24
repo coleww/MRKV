e8633786ac7709e597013ba7054bdf3b1f0c733a
Cole Willsea <coleww@gmail.com>
2014-09-23 20:29:11 -0700

    SICK TESTS BRUH!

--- test/mrkv_test.rb
+++ test/mrkv_test.rb
@@ -2,8 +2,26 @@ require_relative 'test_helper'
 
 class TestMrkvChain <  Minitest::Test
 
-  def test_it_responds_to_new
-    assert Mrkv::Chain.respond_to? :new
+  def setup
+    @chain = Mrkv::Chain.new
+    @chain.add ['i like cats', 'I like pizza', 'I   LiKe    pookie']
+  end
+
+  def test_it_has_ngram_defaulting_to_two
+    assert_equal 2, (Mrkv::Chain.send :ngram)
+  end
+
+  def test_it_turns_arrays_of_lines_into_chains
+    assert_equal ['i like'], (@chain.send :keys)
+  end
+
+  def test_it_generates_stuff_based_on_things_and_dgafs_about_punctuation
+    assert_match /i like \w+/, (@chain.generate 'i like')
+  end
+
+  def test_it_randoms
+    randoms = [0..90].map { |r| @chain.random }
+    assert_equal 3, randoms.uniq
   end
 
 end
